4b6e638ce2ffaa674907679c9ed124b03c8a98ab
MehakMann <mehak@uw.edu>
2019-05-04 16:02:52 -0700

    added parser

--- src/Parser.java
+++ src/Parser.java
@@ -1,23 +1,48 @@
 import java.util.*;
 import java.io.*;
+import java.sql.Timestamp;
 
 public class Parser {
 
     public static void main(String[] args) throws FileNotFoundException{
-        File theFile = new File("../testfiles/TestFile_10.csv");
+        File theFile = new File("./testfiles/TestFile_10.csv");
         Scanner scanFile = new Scanner(theFile);
-        organize(scanFile);
-
+        List<FinancialEntry> list = organize(scanFile);
+        for (int i = 0; i < list.size(); i ++){
+            System.out.println(list.get(i));
+        }
 
     }
 
-    public static List<Object> organize(Scanner theScanner){
-        // have not implemented
-        throw new RuntimeException();
+    public static List<FinancialEntry> organize(Scanner theScanner){
+        List<FinancialEntry> financialEntries = new ArrayList<>();
+        theScanner.nextLine();
+        while (theScanner.hasNextLine()){
+            FinancialEntry thisObject = null;
+            String thisLine = theScanner.nextLine();
+            String[] thisAmount = thisLine.split(",", 4);
+            if (Integer.parseInt(thisAmount[2]) > 0){
+                thisObject = new Donation(thisAmount[3], Integer.parseInt(thisAmount[2]), Timestamp.valueOf(thisAmount[1]));
+
+            }
+            else {
+                //thisObject = new Expense(thisAmount[3],Integer.parseInt(thisAmount[2]), Timestamp.valueOf(thisAmount[1]));
+            }
+            financialEntries.add(thisObject);
+        }
+        return financialEntries;
     }
+
+    //  theScanner.next();
+
+
+    // have not implemented
+    //   throw new RuntimeException();
 
     //
     /*
+
+
 
 
      */
