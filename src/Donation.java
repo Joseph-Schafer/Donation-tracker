9fb426090f234b4bcf14819fcc268f4302699d69
Joey Schafer <schaferj@cs.washington.edu>
2019-05-04 14:35:17 -0700

    Changed double to int fields, fixed some typos.

--- src/Donation.java
+++ src/Donation.java
@@ -1,29 +1,29 @@
-import java.util*;
-import java.sql.TIMESTAMP;
+import java.util.*;
+import java.sql.Timestamp;
 
-public class Donation(){
+public class Donation {
 
     private String name;
-    private double total;
-    private TIMESTAMP date;
+    private int total;
+    private Timestamp date;
 
-    public Donation(String name, double amount, TIMESTAMP time ){
+    public Donation(String name, int amount, Timestamp time ){
         this.name = name;
         this.total = amount;
-        date = new TIMESTAMP(time);
+        this.date = time;
     }
 
     public List<DonationChunk> SplitDonation(){
         List<DonationChunk> chunks = new ArrayList<DonationChunk>();
-        double copyOfTotal = this.total;
-        while(copyOfTotal >= 5.0){
+        int copyOfTotal = this.total;
+        while(copyOfTotal >= 5){
             chunks.add(new DonationChunk(this.name, this.date));
+            copyOfTotal -= 5;
         }
 
-
+        if(this.total != 0) {
+            chunks.add(new DonationChunk(this.name, this.date, this.total));
+        }
+        return chunks;
     }
-
-
-
-
 }
--- src/DonationChunk.java
+++ src/DonationChunk.java
@@ -1,18 +1,21 @@
-import java.util*;
+import java.sql.Timestamp;
+import java.util.*;
 
 public class DonationChunk{
 
     private String donator;
-    private TIMESTAMP time;
+    private Timestamp time;
+    private int amount;
 
-    public DonationChunk(String name, TIMESTAMP time){
+    public DonationChunk(String name, Timestamp time){
         this.donator = name;
         this.time = time;
-
     }
 
-
-
-
+    public DonationChunk(String name, Timestamp time, int amount) {
+        this.donator = name;
+        this.time = time;
+        this.amount = amount;
+    }
 
 }
