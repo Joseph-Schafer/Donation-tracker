fe5b581ddaefb1c01a74129f613ee037083390c4
Joey Schafer <schaferj@cs.washington.edu>
2019-05-04 18:34:07 -0700

    Updated DonationTracker to interface with FinanceQueue, restored FinanceQueue code.

--- src/DonationTracker.java
+++ src/DonationTracker.java
@@ -1,15 +1,18 @@
+import org.json.simple.parser.ParseException;
+
 import java.io.*;
 import java.util.*;
 
 
 public class DonationTracker {
 
-    public static void main(String[] args) throws FileNotFoundException{
+    public static void main(String[] args) throws FileNotFoundException, IOException, ParseException {
         File thisData = new File("./testfiles/RevisedTestFile_50.csv");
         Parser thisParser = new Parser(thisData);
         System.out.println(thisParser.getEntries().toString());
         for (FinancialEntry x : thisParser.getEntries()) {
             System.out.println(x);
         }
+        FinanceQueue q = new FinanceQueue(thisParser.getEntries());
     }
 }
